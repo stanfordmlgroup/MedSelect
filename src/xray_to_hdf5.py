54bbb31332b9901eda5f78a57f5de3fe9d9cb666
Akshay Smit <akshaysm@stanford.edu>
2021-04-08 15:27:00 -0700

    added command line args

--- src/xray_to_hdf5.py
+++ src/xray_to_hdf5.py
@@ -12,25 +12,20 @@ import moco.builder as builder
 from moco.su_dataset import SUDataset
 
 ############## ARGUMENTS FOR SUDATASET ########
-parser = argparse.ArgumentParser(description='Predictor Tranform Image')
-parser.add_argument('-a', metavar='ARCH', default='resnet18')
-parser.add_argument('--moco-dim', type=int, default=128)
-parser.add_argument('--moco-k', type=int, default=32768)
-parser.add_argument('--moco-m', default=0.999, type=float)
-parser.add_argument('--moco-t', default=0.07, type=float)
-parser.add_argument('--img-size', dest='img_size', type=int, default=320)
-parser.add_argument('--crop', dest='crop', type=int, default=320)
-parser.add_argument('--rotate', dest='rotate', type=int, default=10)
-parser.add_argument(
-    '--maintain-ratio',
-    dest='maintain_ratio',
-    default=True,
-    action='store_true')
-parser.add_argument('--scale',dest='scale',type=int,default=320)
-parser.add_argument('--clahe',dest='clahe',type=bool,default=False)
-parser.add_argument('--testmode',dest='testmode',type=bool,default=False)
-args = parser.parse_args()
-args.normalization = 'cxr_norm'
+args = argparse.Namespace()
+args.a='resnet18'
+args.clahe=False
+args.crop=320
+args.img_size=320
+args.maintain_ratio=True
+args.moco_dim=128
+args.moco_k=32768
+args.moco_m=0.999
+args.moco_t=0.07
+args.normalization='cxr_norm'
+args.rotate=10
+args.scale=320
+args.testmode=False
 
 ############## MOCO CLASS ######################
 
@@ -40,22 +35,22 @@ class MoCoEmbedding:
        Required images to be tranformed and resized. Load
        checkpoints of Jingbo (epoch 3). May consider changing.'''
 
-    def __init__(self,mode='fc0'):
+    def __init__(self,mode, path):
         '''This function load the encoder model into a required
            mode (pass in strings):
            1. fc0 -> drop the last fc layer, use previous fc layer
            2. fc1 -> use the last fc layer
            3. conv -> drop both fc layers, use last conv layer'''
 
-        path = '/deep/group/aihc-bootcamp-spring2020/cxr_fewer_samples/experiments/jingbo/resnet18_mocov2_20200617-021146_SLURM1534372/'
-        path += "checkpoint_0003.pth.tar"
+        #path = '/deep/group/aihc-bootcamp-spring2020/cxr_fewer_samples/experiments/jingbo/resnet18_mocov2_20200617-021146_SLURM1534372/'
+        #path += "checkpoint_0003.pth.tar"
         checkpoint = torch.load(path, map_location="cpu")
         state_dict = dict((key[7:], value)
                           for (key, value) in checkpoint['state_dict'].items())
         model = builder.MoCo(
             models.__dict__['resnet18'],
 
-            #Parameter from Jingbo's checkpoint - Different from default!!
+            #Parameter from MoCo checkpoint - Different from default!!
             K=49152,
             mlp=True,
             pretrained=False)
@@ -64,7 +59,6 @@ class MoCoEmbedding:
             self.model = model.encoder_q
         elif mode == 'fc0':
 
-            #STRANGE: without flatten(), output size is [x, 512, 1, 1] 
             self.model = torch.nn.Sequential(*list(model.encoder_q.children())[:-1],torch.nn.Flatten())
         else:
             self.model = torch.nn.Sequential(*list(model.encoder_q.children())[:-2],torch.nn.Flatten())
@@ -74,12 +68,13 @@ class MoCoEmbedding:
         '''This function return the embedding of a batch of images'''
         return self.model(img[0])
 
-def xray_to_hdf5(csv_path, out_path):
+def xray_to_hdf5(csv_path, out_path, ckpt_path):
 	"""Convert each Xray in the csv into an embedding, and store them as hdf5.
 	
 	Args:
 	    csv_path (string): path to csv containing all the X-rays
-	    out_path (string): path to desired output file 
+	    out_path (string): path to desired output file
+            ckpt_path (string): path to MoCo checkpoint
 	"""
 	our_task = {'No Finding': 0,
 	            'Enlarged Cardiomediastinum': 1,
@@ -102,7 +97,7 @@ def xray_to_hdf5(csv_path, out_path):
 	our_dataloader = torch.utils.data.DataLoader(test,batch_size=128,num_workers=6)
 
 	all_batches = []
-	moco = MoCoEmbedding('fc0')
+	moco = MoCoEmbedding('fc0', ckpt_path)
 	device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 	moco.model.eval()
 	moco.model.to(device)
@@ -123,6 +118,18 @@ def xray_to_hdf5(csv_path, out_path):
 	
 
 if __name__ == '__main__':
-	xray_to_hdf5('/deep/group/activelearn/data/mimic-cxr/non_holdout_positives.csv',
-		     '/deep/u/akshaysm/temp.hdf5')
+	prs = argparse.ArgumentParser(description='Get MoCo embeddings from X-ray images listed in a csv file and store them in hdf5 format')
+	prs.add_argument('-d', '--data', type=str, nargs='?', required=True,
+			    help='path to csv containing X-ray file paths. The paths to the images should be under the \"Path\" column')
+	prs.add_argument('-o', '--output_path', type=str, nargs='?', required=True, help='path to intended output hdf5 file)')
+	prs.add_argument('-c', '--checkpoint_path', type=str, nargs='?', required=True, help='path to MoCo checkpoint')
+	arguments = prs.parse_args()
+	csv_path = arguments.data
+	out_path = arguments.output_path
+	ckpt_path = arguments.checkpoint_path
+
+	xray_to_hdf5(csv_path, out_path, ckpt_path)
+
+	#xray_to_hdf5('/deep/group/activelearn/data/mimic-cxr/non_holdout_positives.csv',
+	#	     '/deep/u/akshaysm/temp.hdf5')
 
